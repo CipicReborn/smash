cd6225f9949c9f4209125bd71166ded2af0e240f
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2017-09-06 21:24:26 +0200

    added TouchManager

---
+++ Smash/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+    Touch[] currentTouches;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTouches = Input.touches;
+        for (int i = 0; i < Input.touchCount; i++) {
+            Debug.Log(currentTouches[i].fingerId + " : " + currentTouches[i].position);
+        }
+        
+	}
+}
