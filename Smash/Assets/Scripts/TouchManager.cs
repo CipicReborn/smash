f930d8ecaa7a8acc5519b11175f008d4684c765a
Cyprien Larrouy <clarrouy@manzalab.com>
2017-09-07 14:18:41 +0200

    Sandboxing touch inputs

--- Smash/Assets/Scripts/TouchManager.cs
+++ Smash/Assets/Scripts/TouchManager.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class TouchManager : MonoBehaviour {
     Touch[] currentTouches;
-
-	// Use this for initialization
+    
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
         currentTouches = Input.touches;
         for (int i = 0; i < Input.touchCount; i++) {
-            Debug.Log(currentTouches[i].fingerId + " : " + currentTouches[i].position);
+            //Debug.Log(currentTouches[i].fingerId + " : " + currentTouches[i].position);
+            //drawTouchFeedback(i);
+            //if (currentTouches[i].phase == TouchPhase.Ended) {
+
+            //}
         }
         
 	}
+
+    const int m_MAX_TOUCHES = 4;
+    Color[] FeedbackColors = new Color[m_MAX_TOUCHES] { Color.cyan, Color.red, Color.yellow, Color.green };
+
+    void OnDrawGizmos() {
+        for (int i = 0; i < Input.touchCount; i++) {
+            drawTouchFeedback(i);
+        }
+    }
+
+    void drawTouchFeedback (int i) {
+        if (i >= m_MAX_TOUCHES) {
+            Debug.Log("Impossible to feedback this touch <" + i.ToString() + "> because max Touches count is " + m_MAX_TOUCHES.ToString());
+            return;
+        }
+        Gizmos.color = FeedbackColors[i];
+        Gizmos.DrawSphere(convertScreenPosToGamePos(currentTouches[i].position), 1);
+    }
+
+    Vector3 convertScreenPosToGamePos (Vector2 pPosition) {
+        Vector3 position = new Vector3(pPosition.x, pPosition.y, -0.5f);
+        return Camera.main.ScreenToWorldPoint(position);
+    }
 }
