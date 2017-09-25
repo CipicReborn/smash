3cea76ce9f1839a4f7b480590ff9a55e37d5fdf4
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2017-09-25 07:54:37 +0200

    Added Smash Triggered Feedback

---
+++ Smash/Assets/Scripts/SmashTriggerFeedback.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmashTriggerFeedback : MonoBehaviour {
+
+    public void SetController (IController controller) {
+        m_controller = controller;
+    }
+
+    ParticleSystem m_particleSystem;
+    IController m_controller;
+
+    void Start () {
+        m_particleSystem = GetComponent<ParticleSystem>();
+	}
+	
+	
+	void Update () {
+		if (m_controller.IsSmashTriggered() && !m_particleSystem.isEmitting) {
+            m_particleSystem.Play();
+        }
+        else if (!m_controller.IsSmashTriggered() && m_particleSystem.isEmitting) {
+            m_particleSystem.Stop();
+        }
+	}
+}
