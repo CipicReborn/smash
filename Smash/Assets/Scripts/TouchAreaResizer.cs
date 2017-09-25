d15e294c2fa19268abfd1a8bdc229bfbe2a29f1e
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2017-09-25 07:32:41 +0200

    Added component to resize the touchArea to a defined width in centimeters

---
+++ Smash/Assets/Scripts/TouchAreaResizer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchAreaResizer : MonoBehaviour {
+
+    const float INCH_2_CM = 2.54f;
+    float targetWidthCm = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+        float ScreenWidth = Screen.width / Screen.dpi * INCH_2_CM;
+        float targetWidthPixels = targetWidthCm / INCH_2_CM * Screen.dpi;
+        RectTransform rt = GetComponent<RectTransform>();
+        Debug.Log(rt.sizeDelta);
+        rt.sizeDelta = new Vector2(targetWidthPixels, rt.sizeDelta.y);
+        Debug.Log(rt.sizeDelta);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
