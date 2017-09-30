98e976838b720aca747c8f066b6c8d4d25647d0c
Cyprien Larrouy <cyprien.larrouy@gmail.com>
2017-09-30 14:21:58 +0200

    Added LineRendererController and ParticlesController

---
+++ Smash/Assets/Scripts/LineRendererController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererController : MonoBehaviour {
+
+    int size = 20;
+    LineRenderer line;
+    Queue<Vector3> positions;
+    Vector3[] positionsArray;
+
+    private void Start() {
+        line = GetComponent<LineRenderer>();
+        positions = new Queue<Vector3>();
+    }
+
+    private void Update() {
+        positions.Enqueue(transform.position);
+        if (positions.Count > size) {
+            positions.Dequeue();
+        }
+        positionsArray = positions.ToArray();
+        Array.Reverse(positionsArray);
+        line.positionCount = positionsArray.Length;
+        line.SetPositions(positionsArray);
+    }
+}
---
+++ Smash/Assets/Scripts/ReboundParticlesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReboundParticlesController : MonoBehaviour {
+
+    ParticleSystem emitter;
+
+    public void BurstUp(Vector3 pos) {
+        transform.position = pos;
+        transform.rotation = Quaternion.Euler(new Vector3(-90, 0, 0));
+        emitter.Play();
+    }
+    public void BurstDown(Vector3 pos) {
+        transform.position = pos;
+        transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+        emitter.Play();
+    }
+
+
+    void Start () {
+        emitter = GetComponent<ParticleSystem>();
+	}
+
+}
